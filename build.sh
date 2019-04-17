2f9ec1c6a60dca66dbd040b0e3200d6f5802c2b0
jiweijin <jiweijin@baidu.com>
2019-04-17 13:51:32 +0800

    add cordova

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 yarn run build
 
 # copy file to android
-rm -rf ../cordovawebapp/platforms/android/app/src/main/assets/www/static
-rm -rf ../cordovawebapp/platforms/android/app/src/main/assets/www/index.html
-cp -r dist/ ../cordovawebapp/platforms/android/app/src/main/assets/www/
+rm -rf ./cordovawebapp/platforms/android/app/src/main/assets/www/static
+rm -rf ./cordovawebapp/platforms/android/app/src/main/assets/www/index.html
+cp -r dist/ ./cordovawebapp/platforms/android/app/src/main/assets/www/
 
 
 # copy file to ios
-rm -rf ../cordovawebapp/platforms/ios/www/static
-rm -rf ../cordovawebapp/platforms/ios/www/index.html
-cp -r dist/ ../cordovawebapp/platforms/ios/www/
+rm -rf ./cordovawebapp/platforms/ios/www/static
+rm -rf ./cordovawebapp/platforms/ios/www/index.html
+cp -r dist/ ./cordovawebapp/platforms/ios/www/
